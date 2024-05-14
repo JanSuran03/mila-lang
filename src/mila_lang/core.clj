9e23c92c260837f5fba9abc0d9f373572f3dfe5a
Jan Šuráň <suran.orgpad@gmail.com>
2024-05-14 18:14:48 +0200

    Implement building of the AST in the parser.

--- src/mila_lang/core.clj
+++ src/mila_lang/core.clj
@@ -8,15 +8,15 @@
             [clojure.pprint :as pp]
             [mila-lang.lexer.core :as lexer]
             [mila-lang.parser.core :as parser])
-  (:import (java.awt Toolkit)
-           (java.awt.datatransfer StringSelection)))
+  (:import (clojure.lang ExceptionInfo)
+           (java.awt Toolkit)
+           (java.awt.datatransfer StringSelection)
+           (java.io File)))
 
 (defn copy [s]
   (.. (Toolkit/getDefaultToolkit)
       (getSystemClipboard)
       (setContents (StringSelection. s) nil)))
-
-
 
 (def char-at mila-lang.lexer.base/char-at)
 
@@ -52,7 +52,9 @@
                 [lhs (mapv next xs)]))
          (map (fn [[lhs xs]]
                 [lhs
-                 (let [fn-parse-table-functions (into {} (map (fn [[i f]] [i (eval f)])) xs)]
+                 (let [fn-parse-table-functions (into {} (map (fn [[i f]]
+                                                                [i (eval f)]))
+                                                      xs)]
                    (fn [[[[next-token]] :as args]]
                      (if-let [rule-index (get-in expr-parse-table [lhs next-token])]
                        ((fn-parse-table-functions rule-index) args)
@@ -62,6 +64,27 @@
          (into {}))))
 
 (alter-var-root #'m (constantly (create-function-mappings "expr-grammar.edn" "expr-grammar-parse-table.json")))
+(alter-var-root #'m (constantly (create-function-mappings "attributed-grammar.edn" "parse-table.json")))
 
 (defn evaluate-expr [file]
   ((m :S) [(lexer/lex file)]))
+
+(defn gett [filename]
+  (->> (slurp filename)
+       edn/read-string
+       (map first)
+       (str/join "\n")
+       copy))
+
+(defn parse-samples []
+  (let [samples (File. "samples")]
+    (assert (and (.exists samples) (.isDirectory samples)) "Cannot open directory 'samples'")
+    (doseq [^File child (.listFiles samples)
+            :when (and (str/ends-with? (.getName child) ".mila")
+                       (not (str/starts-with? (.getName child) "noparse")))]
+      (println "Parsing" (.getName child))
+      (try (evaluate-expr child)
+           (println "Success!")
+           (catch ExceptionInfo _
+             (binding [*out* *err*]
+               (println "Error parsing" (.getName child))))))))
